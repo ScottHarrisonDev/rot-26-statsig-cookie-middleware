781ff6aa653550c4f3776eeffec4e4c279ab5289
Scott Harrison <github@scottharrisondesign.co.uk>
2024-09-12 16:26:37 +0100

    initial commit

---
+++ worker.ts
@@ -0,0 +1,49 @@
+import { parse } from "cookie"
+
+export interface Env {
+  API_URL: string
+}
+
+const USER_COOKIE_NAME = 'userUUID'
+
+// Node Crypto library is not supported in CF worker so use this quick one liner for now
+const uuidv4 = () => 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => (c === 'x' ? (Math.random() * 16 | 0) : (Math.random() * 16 | 0 & 0x3 | 0x8)).toString(16));
+
+const getUserUUIDFromCookie = (req): string => {
+  const cookies = parse(req.headers.get('Cookie') || '')
+
+  return cookies[USER_COOKIE_NAME]
+}
+
+export default {
+  async fetch(request, env, ctx) {
+
+    let userUUID = getUserUUIDFromCookie(request)
+    let cookieToSet: null | string = null
+
+    const siteUrl = env.SITE_URL; // This is pointing to http://localhost:8788
+
+    if (!userUUID) {
+      userUUID = uuidv4()
+      cookieToSet = `${USER_COOKIE_NAME}=${userUUID}`
+    }
+
+    let response = await fetch(`${siteUrl}${new URL(request.url).pathname}`, {
+      method: 'GET',
+      headers: {
+        'Cookie': `${USER_COOKIE_NAME}=${userUUID}`
+      },
+    });
+
+    let newResponse = new Response(response.body , {
+      status: response.status,
+      headers: response.headers
+    });
+
+    if (cookieToSet) {
+      newResponse.headers.set("Set-Cookie", cookieToSet)
+    }
+
+    return newResponse
+  },
+}
